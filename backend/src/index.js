fcc18ddd23c00a35245fb03da6bb760cc676c29f
piyuusshh <piiussh@gmail.com>
2026-02-17 16:35:27 +0530

    mongodb setup

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import connectDB from './database/db.js'
+
+dotenv.config({
+    path:'./.env'
+})
+
+const app = express()
+
+const port = process.env.PORT || 7000
+
+connectDB()
+.then(()=>{
+app.listen(port,()=>{
+    console.log(`server is running on port no. ${port}`)
+})
+app.on("error",(error)=>{
+    console.log("errrr:",error)
+    throw error
+})
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed : ",err)
+})
+
+
+//importing routes
+import UserRoutes from "./routes/user.routes.js"
+
+//using routes
+app.use("/api/user",UserRoutes)
