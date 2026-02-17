b253f40ccbabc5dc522aa74e7e88816b009e59ba
piyuusshh <piiussh@gmail.com>
2026-02-18 00:30:58 +0530

    initiating browserRouter , routes and route and thier path

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Verify from './pages/Verify';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Home/>}/>
+          <Route path='/login' element = {<Login/>}/>
+          <Route path='/verify' element = {<Verify/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
